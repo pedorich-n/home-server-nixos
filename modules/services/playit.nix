504bdf3626e0fca2786d9a67b0b3839f253636bb
Nikita Pedorich <pedorich.n@gmail.com>
2023-11-02 00:00:19 +0900

    [flake] Update flake.lock; Fix after update

--- modules/services/playit.nix
+++ modules/services/playit.nix
@@ -5,8 +5,8 @@
     group = "playit";
     secretPath = config.age.secrets.playit-secret.path;
     runOverride = {
-      "62884177-5592-45a9-9662-492b42407881" = [{ port = 43000; }]; # Also 43001
-      "c0310a34-1ed3-4c6d-94f8-739c1d6b2f0f" = [{ port = 44080; }];
+      "62884177-5592-45a9-9662-492b42407881".port = 43000; # Also 43001, 43002
+      "c0310a34-1ed3-4c6d-94f8-739c1d6b2f0f".port = 44080; # Also 44081, 44082
     };
   };
 }
