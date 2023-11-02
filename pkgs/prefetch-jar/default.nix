2142506d8a3888a8e395c7a6e0df2c2b0065f42f
Nikita Pedorich <pedorich.n@gmail.com>
2023-11-03 02:27:55 +0900

    Miscellaneous

--- pkgs/prefetch-jar/default.nix
+++ pkgs/prefetch-jar/default.nix
@@ -1,8 +1,9 @@
-{ writeShellScriptBin, nix }:
-writeShellScriptBin "nix-modrinth-prefetch" ''
+{ pkgs, ... }:
+# TODO: use (${lib.getExe' pkgs.nix "nix-prefetch-url"}, ${lib.getExe' pkgs.nix "nix-hash"} once in stable
+pkgs.writeShellScriptBin "nix-modrinth-prefetch" ''
   url=$1
-  path=$(${nix}/bin/nix-prefetch-url --print-path "$url" | tail -1)
-  hash=$(nix-hash --type sha512 --flat "$path")
+  path=$(${pkgs.nix}/bin/nix-prefetch-url --print-path "$url" | tail -1)
+  hash=$(${pkgs.nix}/bin/nix-hash --type sha512 --flat "$path")
 
   echo "fetchurl { url = \"$url\"; sha512 = \"$hash\"; }"
 ''
