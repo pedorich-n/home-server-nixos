e924c27c2290df0107ee63f3fa3471c4133376bf
Nikita Pedorich <pedorich.n@gmail.com>
2024-01-10 00:17:55 +0900

    [homeassistant] Remove old dashboard

--- modules/services/home-automation/homeassistant/default.nix
+++ modules/services/home-automation/homeassistant/default.nix
@@ -14,7 +14,6 @@ in
     (entryFor ./automations.yaml "automations.yaml")
     (entryFor ./configuration.yaml "configuration.yaml")
     (entryFor ./ui_lovelace_minimalist/custom_cards/custom_card_humidifier/custom_card_humidifier.yaml "ui_lovelace_minimalist/custom_cards/custom_card_humidifier/custom_card_humidifier.yaml")
-    (entryFor ./ui_lovelace_minimalist/dashboard/ui-lovelace-custom.yaml "ui_lovelace_minimalist/dashboard/ui-lovelace-custom.yaml")
     (entryFor ./ui_lovelace_minimalist/dashboard/views/ui-lovelace-custom-grid-templated.yaml "ui_lovelace_minimalist/dashboard/views/ui-lovelace-custom-grid-templated.yaml")
     (entryFor ./ui_lovelace_minimalist/dashboard/ui-lovelace-custom-grid.yaml "ui_lovelace_minimalist/dashboard/ui-lovelace-custom-grid.yaml")
     (entryFor ./custom_components/smartir/codes/climate/17000.json "custom_components/smartir/codes/climate/17000.json")
