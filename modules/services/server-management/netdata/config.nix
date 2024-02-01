4ee24229eaee128b123561570181063cf9ae4dcb
Nikita Pedorich <pedorich.n@gmail.com>
2024-02-02 00:57:30 +0900

    [netdata] Use current money guys

--- modules/services/server-management/netdata/config.nix
+++ modules/services/server-management/netdata/config.nix
@@ -69,7 +69,7 @@ let
   prometheusConfig = ''
     jobs:
       - name: Minecraft
-        url: http://192.168.15.10:${toString config.custom.shared-config.ports.minecraft-money-guys-2.tcp.metrics.port}/metrics
+        url: http://192.168.15.10:${toString config.custom.shared-config.ports.minecraft-money-guys-4.tcp.metrics.port}/metrics
         selector:
           deny:
            - jvm_buffer_pool*
