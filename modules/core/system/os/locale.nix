ae000eec81dd93437d168220422bb538716fb481
Nikita Pedorich <pedorich.n@gmail.com>
2024-04-06 13:58:09 +0900

    Fix locale

--- modules/core/system/os/locale.nix
+++ modules/core/system/os/locale.nix
@@ -1,14 +1,35 @@
 let
   defaultLocale = "en_US.UTF-8";
 
-  extraLocaleVars = [ "LC_ADDRESS" "LC_IDENTIFICATION" "LC_MEASUREMENT" "LC_MONETARY" "LC_NUMERIC" "LC_PAPER" "LC_TIME" ];
+  extraLocaleVars = [
+    "LANG"
+
+    "LC_ADDRESS"
+    "LC_IDENTIFICATION"
+    "LC_MEASUREMENT"
+    "LC_MESSAGES"
+    "LC_MONETARY"
+    "LC_NAME"
+    "LC_NUMERIC"
+    "LC_PAPER"
+  ];
   extraLocaleSettings = builtins.listToAttrs (builtins.map (localeVar: { name = localeVar; value = defaultLocale; }) extraLocaleVars);
 in
 {
   i18n = {
     inherit defaultLocale;
-    inherit extraLocaleSettings;
+    extraLocaleSettings = {
+      "LC_TIME" = "en_GB.UTF-8"; # dd/MM/YYYY please
+      "LC_MEASUREMENT" = "en_GB.UTF-8"; # Metric, please
+    } // extraLocaleSettings;
+
+    supportedLocales = [
+      "en_US.UTF-8/UTF-8"
+      "en_GB.UTF-8/UTF-8"
+    ];
   };
+
+
 
   time.timeZone = "Asia/Tokyo";
 }
