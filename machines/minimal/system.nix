b1019b78653d0aa2ca3588bb7c305d810e35db64
Nikita Pedorich <pedorich.n@gmail.com>
2024-04-09 15:59:00 +0900

    Add this flake to the ISO image

--- machines/minimal/system.nix
+++ machines/minimal/system.nix
@@ -1,12 +1,3 @@
-{ modulesPath, ... }: {
-  imports = [
-    "${modulesPath}/installer/cd-dvd/installation-cd-base.nix"
-  ];
-
-  isoImage = {
-    # See https://nixos.wiki/wiki/Creating_a_NixOS_live_CD#Building_faster
-    squashfsCompression = "gzip";
-  };
-
+{
   system.stateVersion = "23.05";
 }
