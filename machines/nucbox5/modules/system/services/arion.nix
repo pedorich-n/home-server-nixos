ddae5bc0af735529a58916baa81b790cef4e5b7e
Nikita Pedorich <pedorich.n@gmail.com>
2024-04-23 01:13:54 +0900

    Add Arion socket dependency

--- machines/nucbox5/modules/system/services/arion.nix
+++ machines/nucbox5/modules/system/services/arion.nix
@@ -2,4 +2,10 @@
   virtualisation.arion = {
     backend = "podman-socket";
   };
+
+  # TODO: remove after https://github.com/NixOS/nixpkgs/pull/305803 is merged
+  systemd.sockets.podman.socketConfig.ListenStream = [
+    "/run/podman/podman.sock"
+    "/run/docker.sock"
+  ];
 }
