ed7c93e09efe626576cba95b35ae59b203bd7343
Nikita Pedorich <pedorich.n@gmail.com>
2024-04-24 21:45:09 +0900

    [authentik] Remove external outpost

--- machines/geekomA5/modules/system/services/server-management/authentik/arion-compose.nix
+++ machines/geekomA5/modules/system/services/server-management/authentik/arion-compose.nix
@@ -118,32 +118,6 @@ in
           depends_on = [ "postgresql" "redis" ];
           restart = "unless-stopped";
         };
-
-        # outpost.service = rec {
-        #   image = "ghcr.io/goauthentik/proxy:${authentikVersion}";
-        #   container_name = "authentik-outpost";
-        #   networks = {
-        #     default = { };
-        #     traefik = {
-        #       ipv4_address = "172.31.0.240";
-        #     };
-        #   };
-        #   environment = defaultEnvs // {
-        #     AUTHENTIK_HOST = "http://authentik-server:9000";
-        #     AUTHENTIK_HOST_BROWSER = "http://authentik.${config.custom.networking.domain}";
-        #     # AUTHENTIK_DEBUG = "true";
-        #   };
-        #   env_file = [ config.age.secrets.authentik_outpost.path ];
-        #   labels = dockerLib.mkTraefikLabels { name = container_name; port = 9000; } // {
-        #     "traefik.http.routers.authentik-outpost.rule" = "HostRegexp(`{subdomain:[a-z0-9-]+}.${config.custom.networking.domain}`) && PathPrefix(`/outpost.goauthentik.io/`)";
-        #     "traefik.http.routers.authentik-outpost.priority" = "15";
-
-        #     "traefik.http.middlewares.authentik.forwardauth.address" = "http://172.31.0.240:9000/outpost.goauthentik.io/auth/traefik";
-        #     "traefik.http.middlewares.authentik.forwardauth.trustForwardHeader" = "true";
-        #     "traefik.http.middlewares.authentik.forwardauth.authResponseHeaders" = "X-authentik-username,X-authentik-groups,X-authentik-email,X-authentik-name,X-authentik-uid,X-authentik-jwt,X-authentik-meta-jwks,X-authentik-meta-outpost,X-authentik-meta-provider,X-authentik-meta-app,X-authentik-meta-version";
-        #   };
-        # };
-
       };
 
     };
