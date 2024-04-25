ffd2c6efa3f444ad28b3bccd2894ec567546e8a0
Nikita Pedorich <pedorich.n@gmail.com>
2024-04-26 00:37:58 +0900

    Simplify secrets; Use machine-specific secrets;

--- machines/nucbox5/modules/system/services/minecraft-servers/default.nix
+++ machines/nucbox5/modules/system/services/minecraft-servers/default.nix
@@ -13,7 +13,7 @@ in
 
     playit = {
       enable = false;
-      secretPath = config.age.secrets.playit_secret_nucbox.path;
+      secretPath = config.age.secrets.playit_secret.path;
       runOverride = {
         "aeadb59e-5a44-49cb-aaca-be17946e0e04".port = portsCfg.tcp.minecraft-money-guys-4-game.port;
         "c50850ff-06fb-4c83-b9e4-4580db92d4be".port = portsCfg.tcp.minecraft-money-guys-4-square-map.port;
