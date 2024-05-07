6fc5d97d61816345054c2a521e7b9c94d4a0e648
Nikita Pedorich <pedorich.n@gmail.com>
2024-05-07 18:49:02 +0900

    Fix nixPath & flake registries

--- modules/core/nix/nix.nix
+++ modules/core/nix/nix.nix
@@ -1,4 +1,10 @@
 { inputs, lib, ... }:
+let
+  #TODO - more (all) inputs?
+  inputsToUse = {
+    inherit (inputs) nixpkgs nixpkgs-unstable;
+  };
+in
 {
   nix = {
     settings = {
@@ -12,8 +18,15 @@
       options = lib.mkDefault "--delete-older-than 14d";
     };
 
-    nixPath = [
-      "nixpkgs=${inputs.nixpkgs}"
-    ];
+
+    # See https://gist.github.com/tpwrules/b0ab69330ff18da1f9842837ef290740
+    # See https://github.com/ryan4yin/nixos-and-flakes-book/blob/d509b20039964d730848b66c46878be4555fe3d3/docs/best-practices/nix-path-and-flake-registry.md
+    channel.enable = false; # remove nix-channel related tools & configs, use flakes instead.
+
+    nixPath = builtins.map (name: "${name}=/etc/nix/inputs/${name}") (builtins.attrNames inputsToUse);
+
+    registry = lib.mapAttrs' (name: input: { inherit name; value = { flake = input; }; }) inputsToUse;
   };
+
+  environment.etc = lib.mapAttrs' (name: input: { name = "/nix/inputs/${name}"; value = { source = input.outPath; }; }) inputsToUse;
 }
