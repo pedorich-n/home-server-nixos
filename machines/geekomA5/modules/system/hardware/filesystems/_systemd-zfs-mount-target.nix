66bfc006600c16469bc72ce2c28a8f0b1a131984
Nikita Pedorich <pedorich.n@gmail.com>
2024-05-19 14:53:30 +0900

    [zfs] Improve mount-target

--- machines/geekomA5/modules/system/hardware/filesystems/_systemd-zfs-mount-target.nix
+++ machines/geekomA5/modules/system/hardware/filesystems/_systemd-zfs-mount-target.nix
@@ -1,19 +1,24 @@
 { config, lib, pkgs }:
 { dataset, restartTimetout ? "15s", numberOfRetries ? 5 }:
 let
-  sanitezedDataset = lib.strings.sanitizeDerivationName dataset;
+  sanitizedDataset = lib.strings.sanitizeDerivationName dataset;
 in
 {
-  "zfs-mounted-${sanitezedDataset}" = {
+  "zfs-mounted-${sanitizedDataset}" = {
     description = "ZFS dataset ${dataset} is mounted";
-    wants = [ "zfs.target" ];
+    requires = [
+      "zfs-mount.service"
+    ];
+    after = [
+      "zfs-mount.service"
+    ];
 
     startLimitBurst = numberOfRetries;
 
     serviceConfig = {
       Type = "oneshot";
       ExecStart = lib.getExe (pkgs.writeShellApplication {
-        name = "zfs-mounted-check-${sanitezedDataset}";
+        name = "zfs-mounted-check-${sanitizedDataset}";
         runtimeInputs = [ config.boot.zfs.package pkgs.gnugrep ];
         text = ''
           zfs get -H mounted ${dataset} | grep "yes"
