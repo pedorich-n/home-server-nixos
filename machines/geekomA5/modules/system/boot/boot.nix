cdabfc8525f5ea43f60c22da8d3bae82c6031c2e
Nikita Pedorich <pedorich.n@gmail.com>
2024-06-04 17:17:45 +0900

    WIP. Improve power

--- machines/geekomA5/modules/system/boot/boot.nix
+++ machines/geekomA5/modules/system/boot/boot.nix
@@ -20,16 +20,21 @@
     kernelModules = [
       "amdgpu" # AMD GPU 
       "kvm-amd" # KVM on AMD Cpus
-      "k10temp" # Temperature monitoring on AMD CPUs
       "zenpower" # AMD ZEN Family CPUs current, voltage, power monitoring
+      "amd-pstate" # AMD CPU performance scaling driver
       "zfs" # ZFS support
     ];
+
     extraModulePackages = [ config.boot.kernelPackages.zenpower ];
+
+    # https://github.com/NixOS/nixos-hardware/blob/7b49d3967613d9aacac5b340ef158d493906ba79/common/cpu/amd/zenpower.nix#L7C7-L7C49
+    blacklistedKernelModules = [ "k10temp" ];
 
     kernelParams = [
       # 8GB
       # NOTE https://openzfs.github.io/openzfs-docs/Performance%20and%20Tuning/Module%20Parameters.html#zfs-arc-max
       "zfs.zfs_arc_max=${builtins.toString (1024 * 1024 * 1024 * 8)}"
+      "amd_pstate=active"
     ];
 
     loader.systemd-boot.enable = true;
--- machines/geekomA5/modules/system/hardware/power.nix
+++ machines/geekomA5/modules/system/hardware/power.nix
@@ -1,6 +1,15 @@
 { pkgs, ... }: {
-  # List governors: `cpupower frequency-info`
-  powerManagement.cpuFreqGovernor = "schedutil";
-
   environment.systemPackages = [ pkgs.linuxKernel.packages.linux_zen.cpupower ];
+
+  services.auto-cpufreq = {
+    enable = true;
+    settings = {
+      charger = {
+        # see available governors by running: cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_available_governors
+        governor = "powersave";
+        #  EPP: see available preferences by running: cat /sys/devices/system/cpu/cpu0/cpufreq/energy_performance_available_preferences
+        energy_performance_preference = "balance_power";
+      };
+    };
+  };
 }
