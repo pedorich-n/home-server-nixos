53ba0aabd564b532f53c22ee3915d8d068529557
Nikita Pedorich <pedorich.n@gmail.com>
2024-06-07 22:35:43 +0900

    Fix minimal ISO root user password warning

--- machines/minimal/modules/system/users.nix
+++ machines/minimal/modules/system/users.nix
@@ -1,3 +1,4 @@
+{ lib, ... }:
 let
   sshKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID8hr31myOgDIgehpr5QlnPQMXNj+PmQ2EC/YvjymHiP pedorich.n@gmail.com";
 in
@@ -7,6 +8,9 @@ in
     users = {
       root = {
         password = "nixos";
+        # Installation base makes the passowrd empty: https://github.com/NixOS/nixpkgs/blob/de986200b9cb45d2210d7b760ec31a317d20933c/nixos/modules/profiles/installation-device.nix#L40
+        # I prefer the explicitly set password here
+        initialHashedPassword = lib.mkForce null;
         openssh.authorizedKeys.keys = [
           sshKey
         ];
