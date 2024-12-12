692f7f02fa3a434fa0dd4c183ec0bf7a5ab545c5
Nikita Pedorich <pedorich.n@gmail.com>
2024-12-12 14:43:09 +0900

    Update blueprints

--- machines/geekomA5/modules/system/services/authentik/_render-blueprints.nix
+++ machines/geekomA5/modules/system/services/authentik/_render-blueprints.nix
@@ -1,11 +1,20 @@
 { jinja2RendererLib }:
 let
   variables = {
-    groups = {
+    # This one is used to group application in Authentik Dashboard
+    applicationGroups = {
       serverManagement = "Server Management";
       homeAutomation = "Home Automation";
       services = "Services";
       media = "Media";
+      mediaManagement = "Media Management";
+    };
+
+    # This one is for access management to applications
+    accessGroups = {
+      serverAdmins = "Server Admins";
+      media = "Media";
+      mediaAdmins = "Media Admins";
     };
   };
 in
