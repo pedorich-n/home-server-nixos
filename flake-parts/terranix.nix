0a01c53b692177d61dbbcaf7c7aac5670b6dc268
Nikita Pedorich <pedorich.n@gmail.com>
2024-12-16 21:28:22 +0900

    Add terranix default config

--- flake-parts/terranix.nix
+++ flake-parts/terranix.nix
@@ -1,78 +1,81 @@
-{ inputs, flake, ... }: {
+{ inputs, flake, flake-parts-lib, lib, ... }: {
   imports = [ inputs.terranix.flakeModule ];
 
-  perSystem = { pkgs, ... }:
-    let
-      mkWrapper = workdir: {
-        extraRuntimeInputs = [ pkgs.gitMinimal ];
-        prefixText = ''
-          ROOT="$(git rev-parse --show-toplevel)"
-          # shellcheck disable=SC1090,SC1091
-          source "$ROOT/$(dirname ${workdir})/.env"
-        '';
-      };
-    in
-    {
-      terranix = {
-        setDevShell = true;
-
-        terranixConfigurations = {
-          tailscale = rec {
-            terraformWrapper = mkWrapper workdir;
-
-            workdir = "./terranix/tailscale/state";
-
-            modules = [
-              { _module.args.flake = flake; }
-            ] ++ flake.lib.loaders.listFilesRecursively { src = ../terranix/tailscale; };
-          };
-
-          arr-stack = rec {
-            terraformWrapper = mkWrapper workdir;
-
-            workdir = "./terranix/arr-stack/state";
-
-            modules = [
-              {
-                _module.args = {
-                  inherit flake;
-                  inherit (inputs) trash-guides;
+  options = {
+    perSystem = flake-parts-lib.mkPerSystemOption ({ pkgs, ... }: {
+      options.terranix = {
+        terranixConfigurations = lib.mkOption {
+          type = lib.types.attrsOf (lib.types.submodule ({ config, ... }: {
+            options = {
+              terraformWrapper = lib.mkOption {
+                type = lib.types.submodule {
+                  config = {
+                    extraRuntimeInputs = [ pkgs.gitMinimal ];
+                    prefixText = lib.mkBefore ''
+                      ROOT="$(git rev-parse --show-toplevel)"
+                      # shellcheck disable=SC1090,SC1091
+                      source "$ROOT/$(dirname ${config.workdir})/.env"
+                    '';
+                  };
                 };
-              }
-            ] ++ flake.lib.loaders.listFilesRecursively { src = ../terranix/arr-stack; };
-          };
+              };
+            };
+          }));
 
-          backblaze = rec {
-            terraformWrapper = {
-              extraRuntimeInputs = [
-                pkgs._1password-cli
-                pkgs.gitMinimal
-              ];
+        };
+      };
+    });
+  };
 
-              prefixText = ''
-                ROOT="$(git rev-parse --show-toplevel)"
-                # shellcheck disable=SC1090,SC1091
-                source "$ROOT/$(dirname ${workdir})/.env"
+  config = {
 
-                op_path="op://Server/Backblaze_Terranix"
-                B2_APPLICATION_KEY=$(op read "''${op_path}/application_key")
-                B2_APPLICATION_KEY_ID=$(op read "''${op_path}/application_key_id")
-                export B2_APPLICATION_KEY B2_APPLICATION_KEY_ID
-              '';
+    perSystem = { pkgs, ... }:
+      {
+        terranix = {
+          setDevShell = true;
+
+          terranixConfigurations = {
+            tailscale = {
+              workdir = "./terranix/tailscale/state";
+
+              modules = [
+                { _module.args.flake = flake; }
+              ] ++ flake.lib.loaders.listFilesRecursively { src = ../terranix/tailscale; };
             };
 
-            workdir = "./terranix/backblaze/state";
+            arr-stack = {
+              workdir = "./terranix/arr-stack/state";
 
-            modules = [
-              {
-                # _module.args = {
-                #   inherit flake;
-                #   inherit (inputs) trash-guides;
-                # };
-              }
-            ] ++ flake.lib.loaders.listFilesRecursively { src = ../terranix/backblaze; };
+              modules = [
+                {
+                  _module.args = {
+                    inherit flake;
+                    inherit (inputs) trash-guides;
+                  };
+                }
+              ] ++ flake.lib.loaders.listFilesRecursively { src = ../terranix/arr-stack; };
+            };
+
+            backblaze = {
+              terraformWrapper = {
+                extraRuntimeInputs = [
+                  pkgs._1password-cli
+                ];
+
+                prefixText = ''
+                  op_path="op://Server/Backblaze_Terranix"
+                  B2_APPLICATION_KEY=$(op read "''${op_path}/application_key")
+                  B2_APPLICATION_KEY_ID=$(op read "''${op_path}/application_key_id")
+                  export B2_APPLICATION_KEY B2_APPLICATION_KEY_ID
+                '';
+              };
+
+              workdir = "./terranix/backblaze/state";
+
+              modules = flake.lib.loaders.listFilesRecursively { src = ../terranix/backblaze; };
+            };
           };
         };
       };
-    };
+  };
 }
