4b10bc234b47e54dab1dc74ed97dc1d001948e52
Nikita Pedorich <pedorich.n@gmail.com>
2024-12-16 19:39:48 +0900

    Add backblaze terranix

--- flake-parts/terranix.nix
+++ flake-parts/terranix.nix
@@ -41,6 +41,37 @@
               }
             ] ++ flake.lib.loaders.listFilesRecursively { src = ../terranix/arr-stack; };
           };
+
+          backblaze = rec {
+            terraformWrapper = {
+              extraRuntimeInputs = [
+                pkgs._1password-cli
+                pkgs.gitMinimal
+              ];
+
+              prefixText = ''
+                ROOT="$(git rev-parse --show-toplevel)"
+                # shellcheck disable=SC1090,SC1091
+                source "$ROOT/$(dirname ${workdir})/.env"
+
+                op_path="op://Server/Backblaze_Terranix"
+                B2_APPLICATION_KEY=$(op read "''${op_path}/application_key")
+                B2_APPLICATION_KEY_ID=$(op read "''${op_path}/application_key_id")
+                export B2_APPLICATION_KEY B2_APPLICATION_KEY_ID
+              '';
+            };
+
+            workdir = "./terranix/backblaze/state";
+
+            modules = [
+              {
+                # _module.args = {
+                #   inherit flake;
+                #   inherit (inputs) trash-guides;
+                # };
+              }
+            ] ++ flake.lib.loaders.listFilesRecursively { src = ../terranix/backblaze; };
+          };
         };
       };
     };
