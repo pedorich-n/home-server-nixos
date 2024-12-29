1270a759acd01e8786b037db11b3d42c5ba14bd7
Nikita Pedorich <pedorich.n@gmail.com>
2024-12-29 20:58:53 +0900

    Improve permissions & folders

--- machines/geekomA5/modules/system/services/data-library/folders.nix
+++ machines/geekomA5/modules/system/services/data-library/folders.nix
@@ -1,45 +1,77 @@
 { config, lib, ... }:
 let
+  storeRoot = "/mnt/store/data-library";
+  externalRoot = "/mnt/external/data-library";
+
   defaultRules = {
     user = config.users.users.user.name;
     group = config.users.users.user.group;
     mode = "0755";
   };
 
-  mkDirectoryRules = {
+  mkCreateDirectoryRule = {
     "d" = defaultRules; # Create a directory
-    "z" = defaultRules; # Set mode/permissions to a directory, in case it already exists
   };
 
-  storeFolders = lib.map (folder: "/mnt/store/data-library/${folder}") [
-    "audiobookshelf"
-    "audiobookshelf/config"
-    "audiobookshelf/metadata"
-  ];
+  mkSetPermissionsRule = {
+    "Z" = defaultRules; # Set mode/permissions recursively to a directory, in case it already exists
+  };
 
-  externalFolders = lib.map (folder: "/mnt/external/data-library/${folder}") [
-    "downloads/usenet"
-    "downloads/usenet/incomplete"
-    "downloads/usenet/complete"
-    "downloads/usenet/complete/tv"
-    "downloads/usenet/complete/movies"
-    "downloads/usenet/complete/audiobooks"
-    "downloads/usenet/complete/prowlarr"
+  foldersToCreate =
+    (lib.map (folder: "${storeRoot}/${folder}") [
+      "audiobookshelf"
+      "audiobookshelf/config"
+      "audiobookshelf/metadata"
 
-    "downloads/torrent"
-    "downloads/torrent/incomplete"
-    "downloads/torrent/complete"
-    "downloads/torrent/complete/tv"
-    "downloads/torrent/complete/movies"
-    "downloads/torrent/complete/audiobooks"
-    "downloads/torrent/complete/prowlarr"
+      "jellyfin"
+      "jellyfin/cache"
+      "jellyfin/config"
 
-    "media"
-    "media/tv"
-    "media/movies"
-    "media/audiobooks"
+      "prowlarr"
+      "prowlarr/config"
+
+      "qbittorrent"
+      "qbittorrent/config"
+
+      "radarr"
+      "radarr/config"
+
+      "sabnzbd"
+      "sabnzbd/config"
+
+      "sonarr"
+      "sonarr/config"
+    ]) ++ (lib.map (folder: "${externalRoot}/${folder}") [
+      "downloads/usenet"
+      "downloads/usenet/incomplete"
+      "downloads/usenet/complete"
+      "downloads/usenet/complete/tv"
+      "downloads/usenet/complete/movies"
+      "downloads/usenet/complete/audiobooks"
+      "downloads/usenet/complete/prowlarr"
+
+      "downloads/torrent"
+      "downloads/torrent/incomplete"
+      "downloads/torrent/complete"
+      "downloads/torrent/complete/tv"
+      "downloads/torrent/complete/movies"
+      "downloads/torrent/complete/audiobooks"
+      "downloads/torrent/complete/prowlarr"
+
+      "media"
+      "media/tv"
+      "media/movies"
+      "media/audiobooks"
+    ]);
+
+  foldersToSetPermissions = [
+    storeRoot
+    externalRoot
   ];
 in
 {
-  systemd.tmpfiles.settings."90-data-library" = lib.foldl' (acc: folder: acc // { ${folder} = mkDirectoryRules; }) { } (storeFolders ++ externalFolders);
+  systemd.tmpfiles.settings = {
+    "90-data-library-create" = lib.foldl' (acc: folder: acc // { ${folder} = mkCreateDirectoryRule; }) { } foldersToCreate;
+    "91-data-library-set" = lib.foldl' (acc: folder: acc // { ${folder} = mkSetPermissionsRule; }) { } foldersToSetPermissions;
+  };
 }
