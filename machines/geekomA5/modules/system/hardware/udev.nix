370e42bf9efe98db7f838fc1a9d3c3830bea3fb2
Nikita Pedorich <pedorich.n@gmail.com>
2025-01-05 23:48:57 +0900

    udev: Use group name from the config

--- machines/geekomA5/modules/system/hardware/udev.nix
+++ machines/geekomA5/modules/system/hardware/udev.nix
@@ -1,9 +1,9 @@
-{
+{ config, ... }: {
   services.udev = {
     enable = true;
     # Use `udevadm info -a /dev/XXX` or `lsusb -v` to get those attrs
     extraRules = '' 
-      SUBSYSTEM=="tty", ATTRS{idVendor}=="10c4", ATTRS{idProduct}=="ea60", MODE="0664", GROUP="zigbee"
+      SUBSYSTEM=="tty", ATTRS{idVendor}=="10c4", ATTRS{idProduct}=="ea60", MODE="0664", GROUP="${config.users.groups.zigbee.name}"
     '';
   };
 }
