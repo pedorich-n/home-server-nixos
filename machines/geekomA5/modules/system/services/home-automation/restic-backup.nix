20917f3a95c317ef2662f0a8fad39f2875144981
Nikita Pedorich <pedorich.n@gmail.com>
2025-01-05 15:29:14 +0900

    Add HA restic backup

---
+++ machines/geekomA5/modules/system/services/home-automation/restic-backup.nix
@@ -0,0 +1,29 @@
+{ lib, ... }:
+{
+  #NOTE - See also global config at
+  #LINK - machines/geekomA5/modules/system/services/restic.nix
+  services.restic.backups = {
+    homeassistant = {
+      paths = lib.map (rel: "/mnt/store/home-automation/homeassistant/${rel}") [
+        "automations"
+        "configuration"
+        "custom_components"
+        "deps"
+        "local"
+        "resources"
+        ".storage"
+        "themes"
+        "ui_lovelace_minimalist"
+        "www"
+        "automations.yaml"
+        "configuration.yaml"
+      ];
+
+      pruneOpts = [
+        "--keep-daily 7"
+        "--keep-weekly 4"
+        "--keep-monthly 6"
+      ];
+    };
+  };
+}
