2b99be7d5433349e5226e7b2d9bb8a4b99ff8056
Nikita Pedorich <pedorich.n@gmail.com>
2025-01-09 22:42:05 +0900

    WIP. Immich userns

--- machines/geekomA5/modules/system/services/immich/containers.nix
+++ machines/geekomA5/modules/system/services/immich/containers.nix
@@ -1,26 +1,29 @@
 { config, containerLib, systemdLib, ... }:
 let
-  user = "${builtins.toString config.users.users.user.uid}:${builtins.toString config.users.groups.${config.users.users.user.group}.gid}";
+  storeRoot = "/mnt/store/immich";
+  externalStoreRoot = "/mnt/external/immich-library";
 
-  storeFor = localPath: remotePath: "/mnt/store/immich/${localPath}:${remotePath}";
+  containerIds = {
+    uid = 1100;
+    gid = 1100;
+  };
 
-  cacheVolumes = [
-    (storeFor "cache/thumbnails" "/usr/src/app/upload/thumbs")
-    (storeFor "cache/profile" "/usr/src/app/upload/profile")
-  ];
+  user = "${builtins.toString containerIds.uid}:${builtins.toString containerIds.gid}";
 
-  uploadVolumes = [
-    "/mnt/external/immich-library:/usr/src/app/upload"
-  ];
-
-  immichVolumes =
-    cacheVolumes ++
-    uploadVolumes ++
-    [
-      "/etc/localtime:/etc/localtime:ro"
-      "${config.custom.services.immich.configPath}:/usr/src/app/custom-config.json"
-    ];
-
+  mappedVolumeForUser = localPath: remotePath:
+    containerLib.mkIdmappedVolume
+      {
+        uidNamespace = containerIds.uid;
+        uidHost = config.users.users.user.uid;
+        uidCount = 1;
+        uidRelative = true;
+        gidNamespace = containerIds.gid;
+        gidHost = config.users.groups.${config.users.users.user.group}.gid;
+        gidCount = 1;
+        gidRelative = true;
+      }
+      localPath
+      remotePath;
 
   sharedEnvs = {
     # https://immich.app/docs/install/environment-variables/
@@ -32,48 +35,48 @@ let
     IMMICH_TELEMETRY_INCLUDE = "all"; # See https://immich.app/docs/features/monitoring#prometheus
   };
 
-  pod = "immich.pod";
   networks = [ "immich-internal.network" ];
 in
 {
   virtualisation.quadlet = {
     networks = containerLib.mkDefaultNetwork "immich";
 
-    pods.immich = {
-      podConfig = { inherit networks; };
-    };
-
     containers = {
       immich-vectordb = {
+        usernsAuto = true;
 
         containerConfig = {
           image = "registry.hub.docker.com/tensorchord/pgvecto-rs:pg14-v0.2.0@sha256:90724186f0a3517cf6914295b5ab410db9ce23190a2d9d0b9dd6463e3fa298f0";
           environments = sharedEnvs;
           environmentFiles = [ config.age.secrets.immich.path ];
           volumes = [
-            (storeFor "postgresql" "/var/lib/postgresql/data")
+            (mappedVolumeForUser "${storeRoot}/postgresql" "/var/lib/postgresql/data")
           ];
-          inherit networks pod user;
+          inherit networks user;
         };
       };
 
       immich-redis = {
+        usernsAuto = true;
+
         containerConfig = {
           image = "registry.hub.docker.com/library/redis:6.2-alpine@sha256:84882e87b54734154586e5f8abd4dce69fe7311315e2fc6d67c29614c8de2672";
           volumes = [
-            (storeFor "redis" "/data")
+            (mappedVolumeForUser "${storeRoot}/redis" "/data")
           ];
-          inherit networks pod user;
+          inherit networks user;
         };
       };
 
       immich-machine-learning = {
         useGlobalContainers = true;
+
         containerConfig = {
           volumes = [
-            (storeFor "cache/machine-learning" "/cache")
+            (mappedVolumeForUser "${storeRoot}/cache/machine-learning" "/cache")
           ];
-          inherit networks pod user;
+          userns = "auto:size=65535";
+          inherit networks user;
         };
 
         unitConfig = systemdLib.requiresAfter
@@ -95,15 +98,22 @@ in
           addGroups = [
             (builtins.toString config.users.groups.render.gid) # For HW Transcoding
           ];
+          userns = "auto:size=65535";
           devices = [
             "/dev/dri:/dev/dri" # HW Transcoding acceleration. See https://immich.app/docs/features/hardware-transcoding
           ];
-          volumes = immichVolumes;
+          volumes = [
+            "/etc/localtime:/etc/localtime:ro"
+            "${config.custom.services.immich.configPath}:/usr/src/app/custom-config.json:ro"
+            (mappedVolumeForUser "${storeRoot}/cache/thumbnails" "/usr/src/app/upload/thumbs")
+            (mappedVolumeForUser "${storeRoot}/cache/profile" "/usr/src/app/upload/profile")
+            (mappedVolumeForUser externalStoreRoot "/usr/src/app/upload")
+          ];
           labels =
             (containerLib.mkTraefikLabels { name = "immich"; port = 2283; }) ++
             (containerLib.mkTraefikMetricsLabels { name = "immich"; port = 8081; addPath = "/metrics"; }) ++
             (containerLib.mkTraefikMetricsLabels { name = "immich-microservices"; port = 8082; addPath = "/metrics"; });
-          inherit networks pod user;
+          inherit networks user;
         };
 
         unitConfig = systemdLib.requiresAfter
