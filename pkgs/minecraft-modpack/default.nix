7c71e515ca4f91e94de39999a6a7d449fc06c4ed
Nikita Pedorich <pedorich.n@gmail.com>
2025-03-14 00:11:58 +0900

    Update modpack

--- pkgs/minecraft-modpack/default.nix
+++ pkgs/minecraft-modpack/default.nix
@@ -1,10 +1,10 @@
 { pkgs, ... }:
 let
-  version = "V1.1.1";
+  version = "V1.1.2";
 in
 pkgs.fetchPackwizModpack {
   pname = "money-guys-exploration";
   inherit version;
   url = "https://gitlab.com/pablo_peraza/moneyguys-explorationrevival/-/raw/${version}/pack.toml";
-  packHash = "sha256-qIRjoKE8F3zNrRjx90dN95qqXSfC9m1Y2dYFCtFhwno=";
+  packHash = "sha256-FzDbtGXpNYAnFpsB5aF3D6NXdYvBVmhydMbMhd1ZgSk=";
 }
