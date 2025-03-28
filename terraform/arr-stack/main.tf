15ad68422b296e2fcd4e221ce99a098f0be0ea0e
Nikita Pedorich <pedorich.n@gmail.com>
2025-03-28 23:09:26 +0900

    WIP. Add more stuff to arr-stack/prowlarr

--- terraform/arr-stack/main.tf
+++ terraform/arr-stack/main.tf
@@ -1,13 +1,20 @@
 module "onepassword" {
   source = "../modules/onepassword"
-  items  = ["Prowlarr", "Prowlarr_Indexers", "Sonarr", "Radarr", "SABNzbd"]
+  items  = ["Prowlarr", "Prowlarr_Indexers", "Sonarr", "Radarr", "SABnzbd"]
+}
+
+module "arr_stack_shared" {
+  source          = "./modules/arr-shared"
+  sabnznd_api_key = module.onepassword.secrets.SABnzbd.API.key
 }
 
 module "prowlarr" {
-  source              = "./modules/prowlarr"
-  indexer_credentials = module.onepassword.secrets.Prowlarr_Indexers
-  base_url            = "http://prowlarr.server.lan" // TODO: turn into a variable
-  prowlarr_api_key    = module.onepassword.secrets.Prowlarr.API.key
-  radarr_api_key      = module.onepassword.secrets.Radarr.API.key
-  sonarr_api_key      = module.onepassword.secrets.Sonarr.API.key
+  source                             = "./modules/prowlarr"
+  indexer_credentials                = module.onepassword.secrets.Prowlarr_Indexers
+  base_url                           = "http://prowlarr.server.lan" // TODO: turn into a variable
+  prowlarr_api_key                   = module.onepassword.secrets.Prowlarr.API.key
+  radarr_api_key                     = module.onepassword.secrets.Radarr.API.key
+  sonarr_api_key                     = module.onepassword.secrets.Sonarr.API.key
+  sabnzbd_download_client_fields     = module.arr_stack_shared.sabnznd_download_client
+  qbittorrent_download_client_fields = module.arr_stack_shared.qbittorrent_download_client
 }
--- terraform/arr-stack/modules/prowlarr/variables.tf
+++ terraform/arr-stack/modules/prowlarr/variables.tf
@@ -2,6 +2,7 @@ variable "base_url" {
   type = string
 }
 
+
 variable "prowlarr_api_key" {
   type      = string
   sensitive = true
@@ -17,8 +18,30 @@ variable "sonarr_api_key" {
   sensitive = true
 }
 
+variable "sabnzbd_download_client_fields" {
+  type = object({
+    enable   = bool
+    priority = number
+    name     = string
+    host     = string
+    port     = number
+    api_key  = string
+    use_ssl  = bool
+  })
+}
+
+variable "qbittorrent_download_client_fields" {
+  type = object({
+    enable   = bool
+    priority = number
+    name     = string
+    host     = string
+    port     = number
+    use_ssl  = bool
+  })
+}
+
 variable "indexer_credentials" {
   type      = map(any)
   sensitive = true
 }
-
