d3b4f4eac3dab6aa107498e35a82e4e482007f08
Nikita Pedorich <pedorich.n@gmail.com>
2025-03-29 12:34:01 +0900

    Reorganize onepassword

--- terraform/modules/onepassword/locals.tf
+++ terraform/modules/onepassword/locals.tf
@@ -1,14 +1,3 @@
-data "onepassword_vault" "homelab" {
-  name = "HomeLab"
-}
-
-data "onepassword_item" "items" {
-  for_each = toset(var.items)
-
-  vault = data.onepassword_vault.homelab.uuid
-  title = each.key
-}
-
 locals {
   secrets = {
     for key, item in data.onepassword_item.items : key => {
