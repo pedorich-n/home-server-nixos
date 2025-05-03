7f678a21f40250e39dd6424619c7d87346708687
Nikita Pedorich <pedorich.n@gmail.com>
2025-05-03 12:02:07 +0900

    Add udev rule to restart zigbee2mqtt

--- machines/geekomA5/modules/system/hardware/udev.nix
+++ machines/geekomA5/modules/system/hardware/udev.nix
@@ -1,9 +1,14 @@
-{ config, ... }: {
+{ config, lib, ... }:
+let
+  systemctl = lib.getExe' config.systemd.package "systemctl";
+in
+{
   services.udev = {
     enable = true;
     # Use `udevadm info -a /dev/XXX` or `lsusb -v` to get those attrs
     extraRules = '' 
       SUBSYSTEM=="tty", ATTRS{idVendor}=="10c4", ATTRS{idProduct}=="ea60", MODE="0660", GROUP="${config.users.groups.zigbee.name}", SYMLINK+="ttyZigbee"
+      SUBSYSTEM=="tty", ATTRS{idVendor}=="10c4", ATTRS{idProduct}=="ea60", ACTION=="add", RUN+="${systemctl} restart zigbee2mqtt.service"
     '';
   };
 }
