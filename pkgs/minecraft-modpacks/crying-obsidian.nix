48b0b8c00e0819f81faa78b64bdc9fe8240add3d
Nikita Pedorich <pedorich.n@gmail.com>
2025-05-27 10:42:29 +0900

    Update modpack

--- pkgs/minecraft-modpacks/crying-obsidian.nix
+++ pkgs/minecraft-modpacks/crying-obsidian.nix
@@ -1,10 +1,10 @@
 { pkgs, ... }:
 let
-  version = "V1.1.1";
+  version = "V1.3.2";
 in
 pkgs.fetchPackwizModpack {
   pname = "crying-obsidian";
   inherit version;
   url = "https://gitlab.com/pablo_peraza/crying-obsidian/-/raw/${version}/pack.toml";
-  packHash = "sha256-ZI36rw4bsjrOq79Cnl9LaHBY5KYdojjHu0UVOgagVWs=";
+  packHash = "sha256-cpPMBy/NuN6AjkuxVns08YwY8AgRvcEv2jWKiVvrs1A=";
 }
