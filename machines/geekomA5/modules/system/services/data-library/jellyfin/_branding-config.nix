799e9622e4c9986a62d63a329f09bd574c9b483d
Nikita Pedorich <pedorich-n@users.noreply.github.com>
2025-05-31 15:03:20 +0900

    Jellyfin SSO (#191)
    * Remove LDAP
    
    * Jellyfin OIDC
    
    * Setup Jellyfin branding

---
+++ machines/geekomA5/modules/system/services/data-library/jellyfin/_branding-config.nix
@@ -0,0 +1,40 @@
+{ pkgs, networkingLib, ... }:
+let
+  xmlFormat = pkgs.formats.xml { };
+in
+# See https://github.com/9p4/jellyfin-plugin-sso 
+xmlFormat.generate "branding.xml" {
+  BrandingOptions = {
+    LoginDisclaimer = ''
+      <form action="${networkingLib.mkUrl "jellyfin"}/sso/OID/start/Authentik">
+        <button class="raised button-submit block emby-button">
+          Sign in with SSO
+        </button>
+      </form>
+    '';
+
+    CustomCss = ''
+      a.raised.emby-button,
+      .loginDisclaimerContainer,
+      .loginDisclaimer {
+          all: unset;    
+      }
+
+      .btnQuick,
+      .btnSelectServer,
+      .btnForgotPassword,
+      a.raised.emby-button,
+      .emby-button.block,
+      .manualLoginForm,
+      .loginDisclaimerContainer,
+      .loginDisclaimer {
+          margin-left: auto;
+          margin-right: auto;
+          margin-bottom: 1em;
+          color: inherit !important;
+      }
+    '';
+
+    SplashscreenEnabled = false;
+  };
+}
