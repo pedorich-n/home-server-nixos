21f83dcbb3675e0b8c14cfddb3ad043c161ddfdc
Nikita Pedorich <pedorich.n@gmail.com>
2025-07-23 07:53:03 +0900

    Update modpack

--- pkgs/minecraft-modpacks/monkegeddoon.nix
+++ pkgs/minecraft-modpacks/monkegeddoon.nix
@@ -1,10 +1,10 @@
 { pkgs, ... }:
 let
-  version = "0.7.2";
+  version = "0.8.0";
 in
 pkgs.fetchPackwizModpack {
   pname = "monkegeddoon";
   inherit version;
   url = "https://gitlab.com/pablo_peraza/monke-abyss/-/raw/${version}/pack.toml";
-  packHash = "sha256-0/f3rFWKxHIVhajtbiVpEMyTkl+vpHoxY+LcoZwzEOk=";
+  packHash = "sha256-aiemnXfebrnzIJXXMEuK3CfozUzE1XPomsjUnyLoUGI=";
 }
