e331cb2f9bfe8f954435090ec51a6bc6e5631314
Nikita Pedorich <pedorich-n@users.noreply.github.com>
2025-07-28 21:18:16 +0900

    Terraform updates (#206)
    * Fix TF backend settings
    
    * Migrate minio state to B2; More DRY

--- terraform/backblaze/providers.tf
+++ terraform/backblaze/providers.tf
@@ -17,19 +17,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket     = var.s3_backend_bucket
-    key        = "homelab/backblaze/terraform.tfstate"
-    access_key = var.s3_backend_application_key_id
-    secret_key = var.s3_backend_application_key
-
-    skip_credentials_validation = true
-    skip_region_validation      = true
-    skip_s3_checksum            = true        # Not supported by B2
-    region                      = "us-east-1" # Doesn't matter, since B2 doesn't support this setting
-
-    endpoints = {
-      s3 = "https://s3.us-east-005.backblazeb2.com"
-    }
+    key = "homelab/backblaze/terraform.tfstate"
   }
 }
 
--- terraform/tailscale/variables.tf
+++ terraform/tailscale/variables.tf
@@ -5,18 +5,3 @@ variable "server_hostname" {
 variable "server_domain" {
   type = string
 }
-
-variable "s3_backend_application_key_id" {
-  type      = string
-  sensitive = true
-}
-
-variable "s3_backend_application_key" {
-  type      = string
-  sensitive = true
-}
-
-variable "s3_backend_bucket" {
-  type      = string
-  sensitive = true
-}
