c43b7d0f5ab2c8b65eb761a13f28318c3ca6aa37
Nikita Pedorich <pedorich-n@users.noreply.github.com>
2025-07-29 23:19:11 +0900

    Flake Partitions (#207)
    * Use flake partitions with submodules
    
    * Update dev

---
+++ flake-parts/partitions.nix
@@ -0,0 +1,60 @@
+{ inputs, ... }: {
+  imports = [
+    inputs.flake-parts.flakeModules.partitions
+  ];
+
+  partitions.dev = {
+    extraInputsFlake = ../dev;
+    module = {
+      imports = [
+        ../dev/flake-module.nix
+      ];
+
+      perSystem = { pkgs, ... }: {
+        treefmt.config = {
+          projectRoot = ../.;
+
+          settings = {
+            global.excludes = [
+              "**/_sources/*"
+              "**/.terraform.lock.hcl"
+            ];
+            formatter = {
+              djlint = {
+                command = pkgs.djlint;
+                options = [
+                  "--profile=jinja"
+                  "--extension=j2"
+                  "--indent=2"
+                  "--preserve-leading-space"
+                  "--preserve-blank-lines"
+                  "--reformat"
+                  "--warn"
+                  "--quiet"
+                ];
+                includes = [ "*.j2" ];
+              };
+            };
+          };
+
+          programs = {
+            shellcheck = {
+              enable = true;
+              excludes = [ "**/.envrc" ];
+            };
+
+            terraform = {
+              enable = true;
+            };
+          };
+        };
+      };
+    };
+  };
+
+  partitionedAttrs = {
+    devShells = "dev";
+    checks = "dev";
+    formatter = "dev";
+  };
+}
--- flake.nix
+++ flake.nix
@@ -1,5 +1,7 @@
 {
   inputs = {
+    self.submodules = true;
+
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     # nixpkgs-netdata.url = "github:rhoriguchi/nixpkgs/netdata";
