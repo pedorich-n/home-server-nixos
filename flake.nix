e4c9d8d0ef7bb1c6cd693d83813e4aa5e7360edc
Nikita Pedorich <pedorich.n@gmail.com>
2025-08-09 21:37:57 +0900

    Update secrets

--- flake.nix
+++ flake.nix
@@ -76,7 +76,7 @@
     };
 
     home-server-nixos-secrets = {
-      url = "git+ssh://git@github.com/pedorich-n/home-server-nixos-secrets?ref=corekeeper";
+      url = "git+ssh://git@github.com/pedorich-n/home-server-nixos-secrets";
       # url = "git+file:///home/pedorich_n/Projects/home-server-nixos-secrets";
       flake = false;
     };
