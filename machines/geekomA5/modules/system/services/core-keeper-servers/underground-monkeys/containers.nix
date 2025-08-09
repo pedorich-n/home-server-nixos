a0e954235a8e108b7c8aea112236fdfbf44a9b09
Nikita Pedorich <pedorich-n@users.noreply.github.com>
2025-08-09 21:40:56 +0900

    Add Corekeeper Dedicated Server (#220)
    * WIP. Added using container
    
    * Update container settings
    
    * Move to services
    
    * Update secrets

---
+++ machines/geekomA5/modules/system/services/core-keeper-servers/underground-monkeys/containers.nix
@@ -0,0 +1,39 @@
+{ config, containerLib, ... }:
+let
+  storeRoot = "/mnt/store/core-keeper-servers/underground-monkeys";
+
+  mappedVolumeForUser = localPath: remotePath:
+    containerLib.mkIdmappedVolume
+      {
+        uidHost = config.users.users.user.uid;
+        gidHost = config.users.groups.${config.users.users.user.group}.gid;
+      }
+      localPath
+      remotePath;
+in
+{
+  virtualisation.quadlet.containers.core-keeper-server = {
+    useGlobalContainers = true;
+    usernsAuto = {
+      enable = true;
+      size = 65535;
+    };
+
+    containerConfig = {
+      environments = {
+        inherit (containerLib.containerIds) PUID PGID;
+
+        WORLD_NAME = "Underground Monkeys";
+        MAX_PLAYERS = "6";
+
+        MODS_ENABLED = "false";
+      };
+      environmentFiles = [ config.sops.secrets."core-keeper/main.env".path ];
+      volumes = [
+        (mappedVolumeForUser "${storeRoot}/server-data" "/home/steam/core-keeper-data")
+        (mappedVolumeForUser "${storeRoot}/server-files" "/home/steam/core-keeper-dedicated")
+      ];
+    };
+  };
+
+}
---
+++ machines/geekomA5/modules/system/services/core-keeper-servers/underground-monkeys/folders.nix
@@ -0,0 +1,35 @@
+{ config, lib, ... }:
+let
+  storeRoot = "/mnt/store/core-keeper-servers";
+
+  defaultRules = {
+    user = config.users.users.user.name;
+    group = config.users.users.user.group;
+    mode = "0700";
+  };
+
+  mkCreateDirectoryRule = {
+    "d" = defaultRules; # Create a directory
+  };
+
+  mkSetPermissionsRule = {
+    "Z" = defaultRules; # Set mode/permissions recursively to a directory, in case it already exists
+  };
+
+  foldersToCreate =
+    lib.map (folder: "${storeRoot}/${folder}") [
+      "underground-monkeys"
+      "underground-monkeys/server-data"
+      "underground-monkeys/server-files"
+    ];
+
+  foldersToSetPermissions = [
+    storeRoot
+  ];
+in
+{
+  systemd.tmpfiles.settings = {
+    "90-core-keeper-create" = lib.foldl' (acc: folder: acc // { ${folder} = mkCreateDirectoryRule; }) { } foldersToCreate;
+    "91-core-keeper-set" = lib.foldl' (acc: folder: acc // { ${folder} = mkSetPermissionsRule; }) { } foldersToSetPermissions;
+  };
+}
