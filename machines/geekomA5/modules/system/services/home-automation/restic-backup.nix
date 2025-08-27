a390c13a6296b9651c60a4bc20fa39974577bbb2
Nikita Pedorich <pedorich.n@gmail.com>
2025-08-27 19:50:46 +0900

    HA: backup DB as well

--- machines/geekomA5/modules/system/services/home-automation/restic-backup.nix
+++ machines/geekomA5/modules/system/services/home-automation/restic-backup.nix
@@ -1,4 +1,11 @@
-{ lib, ... }:
+{
+  config,
+  lib,
+  ...
+}:
+let
+  dbBackupFolder = "/mnt/store/home-automation/homeassistant/db-backup";
+in
 {
   #NOTE - See also global config at
   #LINK - machines/geekomA5/modules/system/services/restic.nix
@@ -6,6 +13,7 @@
     homeassistant = {
       paths = lib.map (rel: "/mnt/store/home-automation/homeassistant/${rel}") [
         "automations"
+        "db-backup"
         "configuration"
         "custom_components"
         "deps"
@@ -24,6 +32,18 @@
         "--keep-weekly 4"
         "--keep-monthly 3"
       ];
+
+      backupPrepareCommand = ''
+        mkdir -p "${dbBackupFolder}"
+        set -o allexport; source ${config.sops.secrets."home-automation/postgresql.env".path}; set +o allexport
+
+        ${lib.getExe config.virtualisation.podman.package} exec --tty homeassistant-postgresql \
+        pg_dumpall --username ''${POSTGRES_USER} --clean --if-exists > "${dbBackupFolder}/backup.sql"
+      '';
+
+      backupCleanupCommand = ''
+        rm -r "${dbBackupFolder}"
+      '';
     };
   };
 }
