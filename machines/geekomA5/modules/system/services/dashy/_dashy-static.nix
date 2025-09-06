5cab1134f8d9b1b0188ee3f89d27b2dc041a7603
Nikita Pedorich <pedorich.n@gmail.com>
2025-09-07 00:07:49 +0900

    Add basic Dashy implementation

---
+++ machines/geekomA5/modules/system/services/dashy/_dashy-static.nix
@@ -0,0 +1,151 @@
+{
+  pkgs,
+  lib,
+  networkingLib,
+  ...
+}:
+
+let
+  capitilize =
+    word:
+    let
+      firstChar = builtins.substring 0 1 word;
+      restOfWord = builtins.substring 1 (-1) word;
+      upperFirstChar = lib.strings.toUpper firstChar;
+    in
+    "${upperFirstChar}${restOfWord}";
+
+  mkEntry =
+    {
+      slug,
+      title ? capitilize slug,
+      iconName ? slug,
+      iconLink ? "https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/${iconName}.png",
+    }:
+    {
+      inherit title;
+      url = networkingLib.mkUrl slug;
+      icon = iconLink;
+      target = "newtab";
+    };
+
+  # LINK - https://dashy.to/docs/configuring/
+  dashySettings = {
+    pageInfo = {
+      title = "Dashy";
+    };
+
+    appConfig = {
+      disableContextMenu = true;
+
+      hideComponents = {
+        hideSearch = true;
+      };
+    };
+
+    sections = [
+      {
+        name = "Media";
+        icon = "mdi-multimedia";
+        items = [
+          (mkEntry {
+            slug = "jellyfin";
+          })
+          (mkEntry {
+            slug = "audiobookshelf";
+          })
+          (mkEntry {
+            slug = "immich";
+          })
+          (mkEntry {
+            slug = "ente";
+            iconLink = "https://ente.io/assets/ente-photos-icon-transparent.png";
+          })
+          (mkEntry {
+            slug = "copyparty";
+          })
+          (mkEntry {
+            slug = "maloja";
+          })
+        ];
+      }
+      {
+        name = "Home Automation";
+        icon = "mdi-home-automation";
+        items = [
+          (mkEntry {
+            slug = "homeassistant";
+            title = "Home Assistant";
+            iconName = "home-assistant";
+          })
+          (mkEntry {
+            slug = "zigbee2mqtt";
+            title = "Zigbee2MQTT";
+          })
+        ];
+      }
+      {
+        name = "Office";
+        icon = "mdi-file-document-multiple";
+        items = [
+          (mkEntry {
+            slug = "paperless";
+            iconName = "paperless-ngx";
+          })
+          (mkEntry {
+            slug = "grist";
+          })
+        ];
+      }
+      {
+        name = "Media Management";
+        icon = "mdi-movie-open-settings";
+        items = [
+          (mkEntry {
+            slug = "sonarr";
+          })
+          (mkEntry {
+            slug = "radarr";
+          })
+          (mkEntry {
+            slug = "prowlarr";
+          })
+          (mkEntry {
+            slug = "qbittorrent";
+            title = "qBittorrent";
+          })
+          (mkEntry {
+            slug = "sabnzbd";
+            title = "SABnzbd";
+          })
+          (mkEntry {
+            slug = "multiscrobbler";
+            title = "MultiScrobbler";
+            iconName = "multi-scrobbler";
+          })
+        ];
+      }
+      {
+        name = "Server Management";
+        icon = "mdi-server";
+        items = [
+          (mkEntry {
+            slug = "cockpit";
+          })
+          (mkEntry {
+            slug = "netdata";
+          })
+          (mkEntry {
+            slug = "traefik";
+          })
+          (mkEntry {
+            slug = "lldap";
+            title = "LLDAP";
+          })
+        ];
+      }
+    ];
+  };
+
+in
+pkgs.dashy-ui.override { settings = dashySettings; }
---
+++ machines/geekomA5/modules/system/services/dashy/static-web-server.nix
@@ -0,0 +1,39 @@
+{
+  config,
+  networkingLib,
+  pkgs,
+  ...
+}:
+let
+  portsCfg = config.custom.networking.ports.tcp.dashy;
+
+  dashy-static = pkgs.callPackage ./_dashy-static { inherit networkingLib; };
+in
+{
+  custom.networking.ports.tcp.dashy = {
+    port = 48000;
+    openFirewall = false;
+  };
+
+  services = {
+    static-web-server = {
+      enable = true;
+
+      listen = "127.0.0.1:${portsCfg.portStr}";
+      root = dashy-static;
+    };
+
+    traefik.dynamicConfigOptions.http = {
+      routers.dashy-secure = {
+        entryPoints = [ "web-secure" ];
+        rule = "Host(`${networkingLib.mkDomain "dashy"}`)";
+        service = "dashy-secure";
+      };
+
+      services.dashy-secure = {
+        loadBalancer.servers = [ { url = "http://127.0.0.1:${portsCfg.portStr}"; } ];
+      };
+    };
+  };
+
+}
