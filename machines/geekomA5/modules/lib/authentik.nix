f4016f76023d7a90b71ed1dab528bb994e603191
Nikita Pedorich <pedorich.n@gmail.com>
2025-09-16 18:11:16 +0900

    Remove Authentik lib; Clean-up paperless

--- machines/geekomA5/modules/lib/authentik.nix
+++
@@ -1,6 +0,0 @@
-{ networkingLib, ... }:
-{
-  _module.args.authentikLib = {
-    mkIssuerUrl = application: "${networkingLib.mkUrl "authentik"}/application/o/${application}/.well-known/openid-configuration";
-  };
-}
