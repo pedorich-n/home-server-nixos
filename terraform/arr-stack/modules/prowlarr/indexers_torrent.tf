7c6cc8fb88a156c81424e673a876cda65d9de50b
Nikita Pedorich <pedorich.n@gmail.com>
2025-10-20 18:23:02 +0900

    MAM: Increase seed minimal seed period

--- terraform/arr-stack/modules/prowlarr/indexers_torrent.tf
+++ terraform/arr-stack/modules/prowlarr/indexers_torrent.tf
@@ -55,7 +55,7 @@ locals {
         { name = "searchInFilenames", bool_value = false },
         { name = "useFreeleechWedge", number_value = 0 }, # Don't use freeleech wedge
         { name = "searchLanguages", set_value = [] },
-        { name = "torrentBaseSettings.seedTime", number_value = 4320 }, # 3 days
+        { name = "torrentBaseSettings.seedTime", number_value = 5760 }, # 4 days
         { name = "torrentBaseSettings.appMinimumSeeders", number_value = 3 },
         { name = "mamId", text_value = var.indexer_credentials.MyAnonamouse.mam_id }
       ]
