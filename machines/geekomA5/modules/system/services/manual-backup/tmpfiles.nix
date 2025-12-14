7f6c89739e23bbdae4b4520da0bde1e812a72185
Nikita Pedorich <pedorich.n@gmail.com>
2025-12-14 12:20:52 +0900

    Add manual-backup options

---
+++ machines/geekomA5/modules/system/services/manual-backup/tmpfiles.nix
@@ -0,0 +1,30 @@
+{
+  config,
+  lib,
+  tmpfilesLib,
+  ...
+}:
+let
+  storeRoot = config.custom.manual-backup.root;
+
+  rule = {
+    user = config.custom.manual-backup.owner.user;
+    group = config.custom.manual-backup.owner.group;
+    mode = "0775";
+  };
+
+  foldersToCreate = lib.map (folder: "${storeRoot}/${folder}") [
+    "daylio"
+    "gymrun"
+  ];
+
+  foldersToSetPermissions = [
+    storeRoot
+  ];
+in
+{
+  systemd.tmpfiles.settings = {
+    "90-manual-backup-create" = tmpfilesLib.applyRuleToFolders { "d" = rule; } foldersToCreate;
+    "91-manual-backup-set" = tmpfilesLib.applyRuleToFolders { "Z" = rule; } foldersToSetPermissions;
+  };
+}
