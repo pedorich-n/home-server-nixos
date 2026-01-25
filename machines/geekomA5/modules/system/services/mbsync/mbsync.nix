3047bd527d11e76d88ea5276d4a65aae5706622f
Nikita Pedorich <pedorich.n@gmail.com>
2026-01-25 14:40:19 +0900

    Add mbsync timer

--- machines/geekomA5/modules/system/services/mbsync/mbsync.nix
+++ machines/geekomA5/modules/system/services/mbsync/mbsync.nix
@@ -6,6 +6,11 @@
   custom.services.mbsync = {
     enable = true;
 
+    timerConfig = {
+      OnCalendar = "*-*-* *:00,15,30,45:00"; # Every 15 minutes
+      Persistent = true;
+    };
+
     configFile = config.sops.templates."mbsyncrc".path;
     localMailboxes = [ "/mnt/store/mail/archive" ];
     verbose = true;
