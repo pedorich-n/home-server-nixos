6cc2c7fe86669446569982ba3a842b605bf536c6
Nikita Pedorich <pedorich.n@gmail.com>
2026-02-05 20:59:17 +0900

    Update zfs snapshot settings

--- machines/geekomA5/modules/system/services/filesystems/zfs.nix
+++ machines/geekomA5/modules/system/services/filesystems/zfs.nix
@@ -5,10 +5,10 @@ _: {
       # See all arguments: https://github.com/bdrewery/zfstools/blob/4f6185b15d5f9cc575fe8f1175db6ca04d5a1328/bin/zfs-auto-snapshot#L9-L18
       flags = "--keep-zero-sized-snapshots --parallel-snapshots --utc";
 
-      frequent = 8; # 2 hours, every 15 minutes
-      hourly = 24;
-      daily = 14;
-      weekly = 4;
+      frequent = 12; # 3 hours, every 15 minutes
+      hourly = 48;
+      daily = 30;
+      weekly = 0;
       monthly = 0;
     };
 
