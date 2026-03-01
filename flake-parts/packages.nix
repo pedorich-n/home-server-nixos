d1ff60bcb209724d9130f4baa49a5adc7b9c3cd6
Nikita Pedorich <pedorich.n@gmail.com>
2026-03-01 17:19:48 +0900

    WIP. Update modpack

--- flake-parts/packages.nix
+++ flake-parts/packages.nix
@@ -5,8 +5,7 @@
       packages = {
         update-nvfetcher = pkgs.callPackage ../pkgs/update-nvfetcher { };
 
-        modpack = pkgs.callPackage ../pkgs/minecraft-modpacks/crying-obsidian.nix { };
-        modpack2 = pkgs.callPackage ../pkgs/minecraft-modpacks/monkegeddoon.nix { };
+        modpack = pkgs.callPackage ../pkgs/minecraft-modpacks/monkegeddoon.nix { };
       };
     };
 }
--- pkgs/minecraft-modpacks/monkegeddoon.nix
+++ pkgs/minecraft-modpacks/monkegeddoon.nix
@@ -1,10 +1,12 @@
 { pkgs, ... }:
 let
-  version = "0.8.0";
+  ref = "fix-hashes";
+  version = "0.0.1-alpha";
 in
 pkgs.fetchPackwizModpack {
   pname = "monkegeddoon";
   inherit version;
-  url = "https://gitlab.com/pablo_peraza/monke-abyss/-/raw/${version}/pack.toml";
-  packHash = "sha256-aiemnXfebrnzIJXXMEuK3CfozUzE1XPomsjUnyLoUGI=";
+  # url = "https://gitlab.com/pablo_peraza/monke-abyss/-/raw/${commitHash}/pack.toml";
+  url = "https://gitlab.com/pedorich-n/monke-abyss/-/raw/${ref}/pack.toml";
+  packHash = "sha256-ewIQYYvLRJrMh4KGEWaK1yPuQfDE8qu1u9TfQ1G93kw=";
 }
