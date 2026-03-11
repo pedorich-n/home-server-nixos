a70973be99dceeb9bb2b77c91c04985d3a001713
Nikita Pedorich <pedorich.n@gmail.com>
2026-03-11 19:17:47 +0900

    WIP. Switch to service account token instead of CLI

--- shells/tf.nix
+++ shells/tf.nix
@@ -24,8 +24,7 @@ pkgs.mkShellNoCC {
       ROOT="$(git rev-parse --show-toplevel)"
       export ROOT
 
-      OP_ACCOUNT=$(op account list --format=json | jq -r '.[0] | .user_uuid')
-      export OP_ACCOUNT
+      source "$ROOT/op.env"
 
       echo "Getting values from 1Password..."
       item_json=$(op item get "Backblaze_Terraform_State" --vault "HomeLab" --format json)
@@ -36,13 +35,7 @@ pkgs.mkShellNoCC {
 
       TF_CLI_ARGS_init="-backend-config=\"$ROOT/terraform/backblaze.s3.tfbackend\" -backend-config=\"access_key=''${s3_backend_application_key_id}\" -backend-config=\"secret_key=''${s3_backend_application_key}\" -backend-config=\"bucket=''${s3_backend_bucket}\""
 
-      # Limit parallelism to 3 because 1Password CLI can't handle concurrency
-      parallelism_arg="-parallelism=3"
-
-      TF_CLI_ARGS_plan="$parallelism_arg"
-      TF_CLI_ARGS_apply="$parallelism_arg"
-
-      export TF_CLI_ARGS_init TF_CLI_ARGS_plan TF_CLI_ARGS_apply
+      export TF_CLI_ARGS_init
 
       cd "$ROOT/terraform"
     fi
