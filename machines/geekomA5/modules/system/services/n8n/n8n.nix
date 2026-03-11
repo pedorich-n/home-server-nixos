242a8a9bc6c73654d07406b80ff03fc2534ec9ea
Nikita Pedorich <pedorich.n@gmail.com>
2026-03-11 18:50:37 +0900

    Update webhook url

--- machines/geekomA5/modules/system/services/n8n/n8n.nix
+++ machines/geekomA5/modules/system/services/n8n/n8n.nix
@@ -53,7 +53,7 @@ in
         N8N_HOST = networkingLib.mkDomain "n8n";
         N8N_PORT = portsCfg.port;
         N8N_PROTOCOL = "https";
-        WEBHOOK_URL = networkingLib.mkTunneledUrl "telegram-webhook";
+        WEBHOOK_URL = networkingLib.mkTunneledUrl "n8n";
 
         N8N_PROXY_HOPS = "1";
 
