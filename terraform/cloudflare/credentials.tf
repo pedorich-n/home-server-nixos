97a8b4e33162d579a025745d009e0828fb49f977
Nikita Pedorich <pedorich-n@users.noreply.github.com>
2026-04-11 13:28:46 +0900

    More Cloudflare (#372)
    * Rename file; Add service token
    
    * Add more access policies
    
    * Update comment

--- terraform/cloudflare/credentials.tf
+++ terraform/cloudflare/credentials.tf
@@ -3,6 +3,12 @@ data "cloudflare_zero_trust_tunnel_cloudflared_token" "n8n" {
   tunnel_id  = cloudflare_zero_trust_tunnel_cloudflared.n8n.id
 }
 
+resource "cloudflare_zero_trust_access_service_token" "main" {
+  name       = "Machine to Machine Token"
+  account_id = local.cf_account_id
+  duration   = "${24 * 365 * 2}h" # 2 years
+}
+
 resource "onepassword_item" "n8n_token" {
   vault    = module.onepassword.vault_homelab.uuid
   title    = "Cloudflare_Tunnel_N8N"
@@ -35,4 +41,29 @@ resource "onepassword_item" "n8n_token" {
       })
     }
   }
+}
+
+resource "onepassword_item" "cloudflare_service_token" {
+  vault    = module.onepassword.vault_homelab.uuid
+  title    = "Cloudflare_Service_Token"
+  category = "secure_note"
+
+  tags = ["Managed By Terraform"]
+
+  section {
+    label = "Access"
+
+    field {
+      label = "id"
+      type  = "STRING"
+      value = cloudflare_zero_trust_access_service_token.main.client_id
+    }
+
+    field {
+      label = "token"
+      type  = "CONCEALED"
+      value = cloudflare_zero_trust_access_service_token.main.client_secret
+    }
+  }
+
 }
