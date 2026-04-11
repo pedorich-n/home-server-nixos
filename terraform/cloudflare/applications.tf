21d5075b98bded15100b336142918e27bf190446
Nikita Pedorich <pedorich.n@gmail.com>
2026-04-11 13:14:53 +0900

    Add more access policies

--- terraform/cloudflare/applications.tf
+++ terraform/cloudflare/applications.tf
@@ -14,6 +14,34 @@ resource "cloudflare_zero_trust_access_policy" "bypass_telegram_ips" {
   ]
 }
 
+resource "cloudflare_zero_trust_access_policy" "allow_service_token" {
+  account_id       = local.cf_account_id
+  name             = "Allow Service Token"
+  decision         = "allow"
+  session_duration = "24h"
+
+  include = [{
+    service_token = {
+      token_id = cloudflare_zero_trust_access_service_token.main.id
+    }
+  }]
+}
+
+resource "cloudflare_zero_trust_access_policy" "allow_emails" {
+  account_id       = local.cf_account_id
+  name             = "Allow Emails"
+  decision         = "allow"
+  session_duration = "24h"
+
+  include = [
+    for email in split(",", module.onepassword.secrets.Cloudflare_Tunnels.N8N.allowed_emails) : {
+      email = {
+        email = email
+      }
+    }
+  ]
+}
+
 resource "cloudflare_zero_trust_access_policy" "deny_all" {
   account_id       = local.cf_account_id
   name             = "Deny All"
@@ -25,6 +53,7 @@ resource "cloudflare_zero_trust_access_policy" "deny_all" {
   }]
 }
 
+# Not used for anything in Terraform, but useful for manual access to the Cloudflare dashboard and API.
 resource "cloudflare_zero_trust_access_policy" "bypass_all" {
   account_id       = local.cf_account_id
   name             = "Bypass All"
@@ -46,12 +75,20 @@ resource "cloudflare_zero_trust_access_application" "n8n" {
   session_duration = "0s" # Expire immediately
   policies = [
     {
-      id         = cloudflare_zero_trust_access_policy.bypass_telegram_ips.id
+      id         = cloudflare_zero_trust_access_policy.allow_service_token.id
       precedence = 1
     },
     {
-      id         = cloudflare_zero_trust_access_policy.deny_all.id
+      id         = cloudflare_zero_trust_access_policy.allow_emails.id
       precedence = 2
+    },
+    {
+      id         = cloudflare_zero_trust_access_policy.bypass_telegram_ips.id
+      precedence = 3
+    },
+    {
+      id         = cloudflare_zero_trust_access_policy.deny_all.id
+      precedence = 4
     }
   ]
 }
