c2bf688b83565498d7469bec4e3a41fa517d0227
Nikita Pedorich <pedorich.n@gmail.com>
2026-05-02 16:03:22 +0900

    Fix ciJobs definition; Don't run on push

--- flake-parts/options/ciJobs.nix
+++ flake-parts/options/ciJobs.nix
@@ -3,18 +3,14 @@
   lib,
   ...
 }:
-{
-  options = {
-    flake = flake-parts-lib.mkTransposedPerSystemModule {
-      name = "ciJobs";
-      option = lib.mkOption {
-        type = with lib.types; lazyAttrsOf package;
-        default = { };
-        description = ''
-          Derivations to be built by CI jobs.
-        '';
-      };
-      file = ./ciJobs.nix;
-    };
+flake-parts-lib.mkTransposedPerSystemModule {
+  name = "ciJobs";
+  option = lib.mkOption {
+    type = with lib.types; lazyAttrsOf package;
+    default = { };
+    description = ''
+      Derivations to be built by CI jobs.
+    '';
   };
+  file = ./ciJobs.nix;
 }
