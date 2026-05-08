148e179ef1d0a6f55cc070fff4d8695a857aad1b
Nikita Pedorich <pedorich.n@gmail.com>
2026-05-08 20:51:58 +0900

    Fix deploy-nixos

--- pkgs/tools/deploy-nixos/package.nix
+++ pkgs/tools/deploy-nixos/package.nix
@@ -1,16 +1,18 @@
 {
   flake,
-  lib,
   writeShellApplication,
   deploy-rs,
 }:
 writeShellApplication {
   name = "deploy-nixos";
   meta.description = "Deploy given NixOS configuration to remote machines using deploy-rs";
-  program = ''
+  runtimeInputs = [
+    deploy-rs
+  ];
+  text = ''
     system=$1
     shift 1
 
-    ${lib.getExe deploy-rs} "${flake}#$system" "$@"
+    deploy "${flake}#$system" "$@"
   '';
 }
