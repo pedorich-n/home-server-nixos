cf9ba8e39b2990d429da62bb0c7e8bfddfb5f396
Nikita Pedorich <pedorich.n@gmail.com>
2026-06-01 22:24:33 +0900

    Update to 26.05; Update flake.lock

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
   inputs = {
     self.submodules = true;
 
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     # nixpkgs-netdata.url = "github:rhoriguchi/nixpkgs/netdata";
 
