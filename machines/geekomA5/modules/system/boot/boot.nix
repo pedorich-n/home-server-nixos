a508cf1e3e9a8a9a7e27a9fc4cfc986e8f21eae5
Nikita Pedorich <pedorich.n@gmail.com>
2026-06-06 10:52:27 +0900

    Tune network performance

--- machines/geekomA5/modules/system/boot/boot.nix
+++ machines/geekomA5/modules/system/boot/boot.nix
@@ -2,6 +2,16 @@
   config,
   ...
 }:
+let
+  MiB = 1024 * 1024;
+  GiB = 1024 * MiB;
+
+  SixteenMiB = 16 * MiB;
+
+  sysctlRange =
+    min: default: max:
+    "${toString min} ${toString default} ${toString max}";
+in
 {
   boot = {
     supportedFilesystems = {
@@ -14,7 +24,27 @@
       "zenpower" # AMD ZEN Family CPUs current, voltage, power monitoring
       "amd-pstate" # AMD CPU performance scaling driver
       "zfs" # ZFS support
+      "tcp_bbr" # TCP congestion control algorithm
     ];
+
+    kernel.sysctl = {
+      # Congestion Control: BBR for high-throughput, low-latency video
+      "net.ipv4.tcp_congestion_control" = "bbr";
+      # Fair Queuing is mandatory for BBR's pacing to work correctly
+      "net.core.default_qdisc" = "fq";
+
+      # Increase the maximum receive and send buffer sizes for TCP connections to allow for better performance with high-latency, high-bandwidth connections
+      # Set to 16MiB, which is a common recommendation for high-performance networking.
+      "net.core.rmem_max" = SixteenMiB;
+      "net.core.wmem_max" = SixteenMiB;
+
+      # Increase the default and maximum buffer sizes for TCP inbound connections
+      # 87380 is 65536 * 1.33, which is a common recommendation for the default buffer size and takes into account the TCP/IP overhead
+      "net.ipv4.tcp_rmem" = sysctlRange 4096 87380 SixteenMiB;
+
+      # Increase the default and maximum buffer sizes for TCP outbound connections
+      "net.ipv4.tcp_wmem" = sysctlRange 4096 65536 SixteenMiB;
+    };
 
     extraModulePackages = [ config.boot.kernelPackages.zenpower ];
 
@@ -22,9 +52,8 @@
     blacklistedKernelModules = [ "k10temp" ];
 
     kernelParams = [
-      # 8GB
       # https://openzfs.github.io/openzfs-docs/Performance%20and%20Tuning/Module%20Parameters.html#zfs-arc-max
-      "zfs.zfs_arc_max=${builtins.toString (1024 * 1024 * 1024 * 8)}"
+      "zfs.zfs_arc_max=${toString (8 * GiB)}"
       # https://wiki.archlinux.org/title/CPU_frequency_scaling#amd_pstate
       "amd_pstate=active"
     ];
