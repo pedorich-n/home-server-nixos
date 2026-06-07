ccb559ae54507dbefe1499bf3473b2e01954a6a9
Nikita Pedorich <pedorich.n@gmail.com>
2026-06-07 15:31:51 +0900

    Add Redis firewall settings

--- machines/geekomA5/modules/system/services/music-history/redis.nix
+++ machines/geekomA5/modules/system/services/music-history/redis.nix
@@ -13,10 +13,21 @@ in
     };
   };
 
+  networking.firewall.interfaces."podman+" = {
+    # Allows access to the Redis server from the container
+    allowedTCPPorts = [
+      portsCfg.port
+    ];
+  };
+
   services.redis.servers.multiscrobbler = {
     enable = true;
 
-    bind = "127.0.0.1";
+    bind = "0.0.0.0"; # Listen on all interfaces, so that it can be accessed from the container
     inherit (portsCfg) port;
+
+    settings = {
+      protected-mode = "no";
+    };
   };
 }
