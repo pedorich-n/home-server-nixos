2cb638ce4757407a7822e215de0eafa0333ea9ba
Nikita Pedorich <pedorich.n@gmail.com>
2026-06-07 17:25:23 +0900

    MS: Add MusicBrainz stage

--- machines/geekomA5/modules/system/services/music-history/containers.nix
+++ machines/geekomA5/modules/system/services/music-history/containers.nix
@@ -72,6 +72,9 @@ in
           environments = {
             inherit (containerLib.containerIds) PUID PGID;
             TZ = config.time.timeZone;
+            # NodeJS 20+ uses IPv6 by default. I don't have IPv6 enabled, but for some reason it still tries to use it and fails with ETIMEDOUT
+            # See https://github.com/nodejs/node/issues/54359
+            NODE_OPTIONS = "--network-family-autoselection-attempt-timeout=3000";
           };
           volumes = [
             (containerLib.mkMappedVolumeForUser "${storeRoot}/multi-scrobbler/config" "/config")
--- machines/geekomA5/modules/system/services/music-history/secrets.nix
+++ machines/geekomA5/modules/system/services/music-history/secrets.nix
@@ -17,7 +17,7 @@ in
       file = pkgs.writers.writeJSON "multiscrobbler-config.json" {
         base_url = networkingLib.mkUrl "multiscrobbler";
         logging = {
-          level = "verbose";
+          level = "info";
         };
         cache = {
           valkey = "redis://host.containers.internal:${portsCfg.redis-multiscrobbler.portStr}";
@@ -28,12 +28,28 @@ in
             name = "spotify";
             enable = true;
             type = "spotify";
+            id = "spotify";
             data = {
               clientId = config.sops.placeholder."music-history/multiscrobbler/spotify/client_id";
               clientSecret = config.sops.placeholder."music-history/multiscrobbler/spotify/client_secret";
             };
             options = {
               scrobbleBacklog = true;
+              playTransform = {
+                preCompare = [
+                  {
+                    # if MusicBrainz is successful then do NOT run native,
+                    # only run native if MusicBrainz fails to find a match (onFailure)
+                    type = "musicbrainz";
+                    name = "MusicBrainz";
+                    onSuccess = "stop";
+                    onFailure = "continue";
+                  }
+                  {
+                    type = "native";
+                  }
+                ];
+              };
             };
           }
         ];
@@ -52,6 +68,7 @@ in
             name = "maloja";
             enable = true;
             type = "maloja";
+            id = "maloja";
             configureAs = "client";
             data = {
               url = "http://maloja:42010";
@@ -63,11 +80,46 @@ in
             name = "koito";
             enable = true;
             type = "koito";
+            id = "koito";
             configureAs = "client";
             data = {
               token = config.sops.placeholder."music-history/multiscrobbler/koito/api_key";
               username = config.sops.placeholder."music-history/multiscrobbler/koito/username";
               url = networkingLib.mkUrl "koito";
+            };
+          }
+        ];
+
+        transformers = [
+          {
+            # From https://docs.multi-scrobbler.app/configuration/transforms/musicbrainz/#sensible-default-1
+            name = "MusicBrainz";
+            type = "musicbrainz";
+            data = {
+              apis = [
+                {
+                  contact = "pedorich.n@gmail.com";
+                }
+              ];
+            };
+            defaults = {
+              releaseStatusPriority = [
+                "official"
+              ];
+              releaseGroupPrimaryTypePriority = [
+                "album"
+                "single"
+                "ep"
+              ];
+              releaseCountryPriority = [
+                "XW" # Worldwide
+              ];
+              searchArtistMethod = "native";
+              searchOrder = [
+                "isrc" # International Standard Recording Code
+                "basic" # Combination of artist, album, track names
+                "artist" # Attempt to extract artist names from the track
+              ];
             };
           }
         ];
