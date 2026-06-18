02a72ea43956ac61a6c6e0767707b7bfb2e8323a
Nikita Pedorich <pedorich.n@gmail.com>
2026-06-18 23:45:08 +0900

    Drop tofu minio

--- terraform/minio/providers.tf
+++
@@ -1,6 +0,0 @@
-provider "minio" {
-  minio_server   = "storage.${var.server_domain}"
-  minio_ssl      = true
-  minio_user     = module.onepassword.secrets.Minio.Root_Credentials.user
-  minio_password = module.onepassword.secrets.Minio.Root_Credentials.password
-}
--- terraform/minio/variables.tf
+++
@@ -1,3 +0,0 @@
-variable "server_domain" {
-  type = string
-}
