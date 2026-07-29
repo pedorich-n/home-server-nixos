718a963fd06a0c33d76e997bfd547db183cec2ee
Nikita Pedorich <pedorich.n@gmail.com>
2026-07-29 19:38:56 +0900

    Update lidarr settings

--- terraform/arr-stack/modules/lidarr/data.tf
+++ terraform/arr-stack/modules/lidarr/data.tf
@@ -1,7 +1,3 @@
 data "lidarr_quality_profile" "standard" {
   name = "Standard"
 }
-
-data "lidarr_metadata_profile" "standard" {
-  name = "Standard"
-}
--- terraform/arr-stack/modules/lidarr/main.tf
+++ terraform/arr-stack/modules/lidarr/main.tf
@@ -1,7 +1,29 @@
+resource "lidarr_metadata_profile" "standard" {
+  name = "Standard"
+  # Data from API
+  primary_album_types = [
+    0, # Album
+    1, # EP
+    2, # Single
+  ]
+  secondary_album_types = [
+    0,  # Studio
+    1,  # Compilation
+    2,  # Soundtrack
+    3,  # Spokenword
+    7,  # Remix
+    10, # Demo
+  ]
+
+  release_statuses = [
+    0, # Official
+  ]
+}
+
 resource "lidarr_root_folder" "root" {
   name                    = "Music"
   quality_profile_id      = data.lidarr_quality_profile.standard.id
-  metadata_profile_id     = data.lidarr_metadata_profile.standard.id
+  metadata_profile_id     = lidarr_metadata_profile.standard.id
   monitor_option          = "none"
   new_item_monitor_option = "none"
   path                    = "/mnt/external/data-library/media/music"
@@ -10,8 +32,8 @@ resource "lidarr_root_folder" "root" {
 resource "lidarr_naming" "naming" {
   rename_tracks              = true
   replace_illegal_characters = true
-  standard_track_format      = "{[Release Year] }{Album CleanTitle}/{Track ArtistCleanName} - {Album CleanTitle} - {track:00} - {Track CleanTitle}"
-  multi_disc_track_format    = "{[Release Year] }{Album CleanTitle}/{Medium Format}/{medium:00}/{Track ArtistCleanName} - {Album CleanTitle} - {track:00} - {Track CleanTitle}"
+  standard_track_format      = "{[Release Year] }{Album CleanTitle}/{Track ArtistCleanName} - {[Release Year] }{Album CleanTitle} - {track:00} - {Track CleanTitle}"
+  multi_disc_track_format    = "{[Release Year] }{Album CleanTitle}/{Medium Format}/{medium:00}/{Track ArtistCleanName} - {[Release Year] }{Album CleanTitle} - {track:00} - {Track CleanTitle}"
   artist_folder_format       = "{Artist CleanNameThe}"
 }
 
