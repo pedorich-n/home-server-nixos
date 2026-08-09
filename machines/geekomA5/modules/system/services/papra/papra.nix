36c5dd49e2c51397eeeb36e2b4b5a3ed8b5b0cf5
Nikita Pedorich <pedorich.n@gmail.com>
2026-08-09 13:05:42 +0900

    Add basic Papra

---
+++ machines/geekomA5/modules/system/services/papra/papra.nix
@@ -0,0 +1,88 @@
+{
+  config,
+  lib,
+  systemdLib,
+  networkingLib,
+  ...
+}:
+let
+  portsCfg = config.custom.networking.ports.tcp.papra;
+in
+{
+  custom = {
+    networking.ports.tcp.papra = {
+      port = 33000;
+      openFirewall = false;
+    };
+
+    services.caddy.hosts.papra = {
+      upstream = "http://127.0.0.1:${portsCfg.portStr}";
+    };
+  };
+
+  systemd.services.papra = {
+    serviceConfig = {
+      EnvironmentFile = [
+        config.sops.secrets."papra/main.env".path
+        config.sops.templates."papra/oidc.env".path
+      ];
+
+      # Hardening
+      # NoNewPrivileges = true;
+      # MemoryDenyWriteExecute = true;
+      # LockPersonality = true;
+      # PrivateDevices = true;
+      # PrivateTmp = true;
+      # PrivateUsers = true;
+      # PrivateMounts = true;
+      # DynamicUser = true;
+      # ProtectHome = true;
+      # # ProtectSystem = "strict";
+      # ProtectKernelModules = true;
+      # ProtectKernelTunables = true;
+      # ProtectKernelLogs = true;
+      # ProtectControlGroups = true;
+      # ProtectClock = true;
+      # RestrictSUIDSGID = true;
+      # RestrictNamespaces = true;
+      # RestrictRealtime = true;
+      # RestrictAddressFamilies = [
+      #   "AF_INET"
+      #   "AF_INET6"
+      # ];
+    };
+
+    unitConfig = lib.mkMerge [
+      (systemdLib.requisiteAfter [
+        "zfs.target"
+      ])
+    ];
+  };
+
+  services.papra = {
+    enable = true;
+
+    environment = {
+      SERVER_HOSTNAME = "127.0.0.1";
+      PORT = portsCfg.port;
+      APP_BASE_URL = networkingLib.mkLocalUrl "papra";
+
+      DOCUMENT_STORAGE_DRIVER = "filesystem";
+      DOCUMENT_STORAGE_FILESYSTEM_ROOT = "/mnt/external/papra-library";
+
+      CONTENT_EXTRACTION_STRATEGY = "internal";
+      DOCUMENTS_OCR_LANGUAGES = "eng,ukr,jpn";
+
+      AUTH_IS_REGISTRATION_ENABLED = "false";
+      AUTH_IS_PASSWORD_RESET_ENABLED = "false";
+      AUTH_IS_EMAIL_VERIFICATION_REQUIRED = "false";
+      AUTH_FIRST_USER_AS_ADMIN = "true";
+      AUTH_IP_ADDRESS_HEADERS = "X-Forwarded-For";
+      AUTH_PROVIDERS_EMAIL_IS_ENABLED = "false";
+
+      INGESTION_FOLDER_IS_ENABLED = "false";
+
+      AI_IS_ENABLED = "false";
+    };
+  };
+}
--- machines/geekomA5/modules/system/services/papra/secrets.nix
+++ machines/geekomA5/modules/system/services/papra/secrets.nix
@@ -10,12 +10,12 @@ in
 {
   sops.templates = {
     "papra/oidc.env" = {
-      # owner = config.services.papra.user;
-      # group = config.services.papra.group;
-      file = keyValueFomat.generate "paperless-oidc.env" {
+      owner = config.services.papra.user;
+      group = config.services.papra.group;
+      file = keyValueFomat.generate "papra-oidc.env" {
         AUTH_PROVIDERS_CUSTOMS = builtins.toJSON [
           {
-            prodiverId = "authelia";
+            providerId = "authelia";
             providerName = "Authelia";
             providerIconUrl = "https://www.authelia.com/images/branding/logo-cropped.png";
             type = "oidc";
