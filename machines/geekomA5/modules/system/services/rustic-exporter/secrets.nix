b783d8fc1dc8fe7a3801d82f4f8ef0c13ee468ed
Nikita Pedorich <pedorich.n@gmail.com>
2026-08-16 15:20:13 +0900

    Add basic rustic-exporter config

---
+++ machines/geekomA5/modules/system/services/rustic-exporter/secrets.nix
@@ -0,0 +1,38 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+let
+  mkConfigEntry =
+    name:
+    let
+      slug = lib.toLower name;
+    in
+    {
+      inherit name;
+      repository = "opendal:s3";
+      passwordFile = config.sops.secrets."restic/${slug}/password".path;
+      options = {
+        endpoint = config.sops.placeholder."restic/${slug}/endpoint";
+        access_key_id = config.sops.placeholder."restic/${slug}/access_key_id";
+        secret_access_key = config.sops.placeholder."restic/${slug}/secret_access_key";
+        bucket = config.sops.placeholder."restic/${slug}/bucket_name";
+        region = "auto"; # Not used by Backblaze B2, but required by the S3 API
+        root = "/";
+      };
+    };
+in
+{
+  sops.templates = {
+    "rustic-exporter/config.toml" = {
+      file = pkgs.writers.writeTOML "rustic-exporter-config.toml" {
+        backup = [
+          (mkConfigEntry "Audiobookshelf")
+          (mkConfigEntry "HomeAssistant")
+        ];
+      };
+    };
+  };
+}
