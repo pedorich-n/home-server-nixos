dd6259b23c3ae6dbbebbff541d58d8b88d78fc6a
Nikita Pedorich <pedorich.n@gmail.com>
2026-08-21 22:59:24 +0900

    Make rustic-exporter into a module

--- machines/geekomA5/modules/system/services/rustic-exporter/rustic-exporter.nix
+++ machines/geekomA5/modules/system/services/rustic-exporter/rustic-exporter.nix
@@ -1,26 +1,9 @@
 {
   config,
-  pkgs,
-  lib,
   ...
 }:
 let
   portsCfg = config.custom.networking.ports.tcp.rustic-exporter;
-
-  args = [
-    "--host"
-    "127.0.0.1"
-    "--port"
-    portsCfg.portStr
-    "--output"
-    "text"
-    "--log-level"
-    "info"
-    "--interval"
-    "300"
-    "--config"
-    "\${CONFIG_FILE}"
-  ];
 in
 {
   custom = {
@@ -29,63 +12,19 @@ in
       openFirewall = false;
     };
 
-    services.caddy.metrics.routes = {
-      rustic-exporter = {
-        url = "http://127.0.0.1:${portsCfg.portStr}";
+    services = {
+      caddy.metrics.routes = {
+        rustic-exporter = {
+          url = "http://127.0.0.1:${portsCfg.portStr}";
+        };
       };
-    };
-  };
 
-  systemd.services.rustic-exporter = {
-    enable = true;
-    description = "Restic metrics exporter for Prometheus";
-    after = [ "network.target" ];
-    wants = [ "network.target" ];
-    wantedBy = [ "multi-user.target" ];
+      rustic-exporter = {
+        enable = true;
+        inherit (portsCfg) port;
 
-    environment = {
-      CONFIG_FILE = "%d/config.toml";
-    };
-
-    serviceConfig = {
-      ExecStart = "${lib.getExe pkgs.rustic-exporter} ${lib.concatStringsSep " " args}";
-      LoadCredential = [
-        "config.toml:${config.sops.templates."rustic-exporter/config.toml".path}"
-      ];
-      Restart = "on-failure";
-      RestartSec = 5;
-
-      # Hardening
-      CapabilityBoundingSet = "";
-      DynamicUser = true;
-      LockPersonality = true;
-      MemoryDenyWriteExecute = true;
-      NoNewPrivileges = true;
-      PrivateDevices = true;
-      PrivateTmp = true;
-      PrivateUsers = true;
-      ProtectClock = true;
-      ProtectControlGroups = true;
-      ProtectHome = true;
-      ProtectHostname = true;
-      ProtectKernelLogs = true;
-      ProtectKernelModules = true;
-      ProtectKernelTunables = true;
-      ProtectProc = "invisible";
-      RemoveIPC = true;
-      RestrictAddressFamilies = [
-        "AF_INET"
-        "AF_INET6"
-      ];
-      RestrictNamespaces = true;
-      RestrictRealtime = true;
-      RestrictSUIDSGID = true;
-      SystemCallArchitectures = "native";
-      SystemCallFilter = [
-        "@system-service"
-        "~@privileged"
-      ];
-      UMask = "0077"; # 600 for files, 700 for dirs
+        configFile = config.sops.templates."rustic-exporter/config.toml".path;
+      };
     };
   };
 }
