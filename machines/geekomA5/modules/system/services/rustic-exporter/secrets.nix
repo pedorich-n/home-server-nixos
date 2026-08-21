c73859e563cc4103b911cc212b5d23f9904f2b65
Nikita Pedorich <pedorich.n@gmail.com>
2026-08-21 22:20:56 +0900

    Produce metrics for all backup automatically

--- machines/geekomA5/modules/system/services/rustic-exporter/secrets.nix
+++ machines/geekomA5/modules/system/services/rustic-exporter/secrets.nix
@@ -5,33 +5,36 @@
   ...
 }:
 let
-  mkConfigEntry =
-    name:
-    let
-      slug = lib.toLower name;
-    in
-    {
-      inherit name;
-      repository = "opendal:s3";
-      password = config.sops.placeholder."restic/${slug}/password";
-      options = {
-        endpoint = config.sops.placeholder."restic/${slug}/endpoint";
-        access_key_id = config.sops.placeholder."restic/${slug}/access_key_id";
-        secret_access_key = config.sops.placeholder."restic/${slug}/secret_access_key";
-        bucket = config.sops.placeholder."restic/${slug}/bucket_name";
-        region = "auto"; # Not used by Backblaze B2, but required by the S3 API
-        root = "/";
-      };
+  mkConfigEntry = name: {
+    inherit name;
+    repository = "opendal:s3";
+    password = config.sops.placeholder."restic/${name}/password";
+    options = {
+      endpoint = config.sops.placeholder."restic/${name}/endpoint";
+      access_key_id = config.sops.placeholder."restic/${name}/access_key_id";
+      secret_access_key = config.sops.placeholder."restic/${name}/secret_access_key";
+      bucket = config.sops.placeholder."restic/${name}/bucket_name";
+      region = "auto"; # Not used by Backblaze B2, but required by the S3 API
+      root = "/";
     };
+  };
+
+  excludedBackups = {
+    # These services have been retired, so they have no new snapshots.
+    # But for archival purposes they're still listed in the secrets and configs.
+    maloja = true;
+    paperless = true;
+  };
+  resticBackups = lib.filter (backup: !excludedBackups.${backup} or false) (lib.attrNames config.services.restic.backups);
 in
 {
   sops.templates = {
     "rustic-exporter/config.toml" = {
+      restartUnits = [
+        config.systemd.services.rustic-exporter.name
+      ];
       file = pkgs.writers.writeTOML "rustic-exporter-config.toml" {
-        backup = [
-          (mkConfigEntry "Audiobookshelf")
-          (mkConfigEntry "HomeAssistant")
-        ];
+        backup = lib.map mkConfigEntry resticBackups;
       };
     };
   };
