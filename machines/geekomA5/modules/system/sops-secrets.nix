0e2f96c0603d0c74276f12a6f0d8af8c9313c3d7
Nikita Pedorich <pedorich.n@gmail.com>
2026-08-22 00:12:26 +0900

    Drop unused secrets

--- machines/geekomA5/modules/system/sops-secrets.nix
+++ machines/geekomA5/modules/system/sops-secrets.nix
@@ -416,7 +416,6 @@ in
 
         "sonarr/api/key" = { };
         "radarr/api/key" = { };
-        "prowlarr/api/key" = { };
         "homeassistant/api/key" = { };
         "paperless/api/key" = { };
       }
