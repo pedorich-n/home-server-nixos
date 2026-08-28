7fdc4fb430323dc765f69f28be432480803dccd6
Nikita Pedorich <pedorich.n@gmail.com>
2026-08-28 23:45:17 +0900

    Rework ciJobs

--- flake-parts/ciJobs.nix
+++ flake-parts/ciJobs.nix
@@ -7,16 +7,17 @@
   flake.ciJobs =
     let
       geekomA5 = flake.nixosConfigurations.geekomA5;
-      minecraftConfig = geekomA5.config.services.minecraft-servers.servers."monkey-guys-1";
-    in
-    lib.mkMerge [
-      {
-        "${geekomA5.pkgs.stdenv.hostPlatform.system}" = {
-          n8n = geekomA5.config.services.n8n.package;
-          netdata = geekomA5.config.services.netdata.package;
-          minecraftServer = minecraftConfig.package;
-          minecraftModpack = minecraftConfig.package.passthru.modpack;
+
+      packageIfServiceEnabled =
+        service:
+        lib.mkIf geekomA5.config.services.${service}.enable {
+          ${service} = geekomA5.config.services.${service}.package;
         };
-      }
-    ];
+    in
+    {
+      "${geekomA5.pkgs.stdenv.hostPlatform.system}" = lib.mkMerge [
+        (packageIfServiceEnabled "netdata")
+        (packageIfServiceEnabled "n8n")
+      ];
+    };
 }
